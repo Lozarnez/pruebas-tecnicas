4b935e05356e3ab3bac6f14c25c5e2c8e1dea34c
Juan Pablo Loaiza <jploaiza@dicalzeus.com>
2024-05-08 10:13:07 -0700

    feat: prueba de manejo de estado con React

---
+++ toggle-text/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <button>Click on me!</button>
+      <h2>¡Hola programador!</h2>
+    </div>
+  );
+}
---
+++ toggle-text/src/Instructions.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Instructions = () => {
+  return (
+    <>
+      <h1>Instrucciones</h1>
+      <ul>
+        <li>
+          Trabaja en el archivo <b>/App.js</b>
+        </li>
+        <li>Agrega un botón que al hacer click oculte / muestre el texto</li>
+      </ul>
+    </>
+  );
+};
