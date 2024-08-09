7692dcd90eb17570c9a5bfd2e8be5362b10dd1ad
Juan Pablo Loaiza Martínez <juanpa@MacBook-Air-de-Juan.local>
2024-08-09 13:03:54 -0700

    Prueba casi terminada Alan (me puse nervioso)

--- data-fetch/src/App.js
+++ data-fetch/src/App.js
@@ -1,6 +1,36 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 export default function App() {
+
+  const [datos, setDatos] = useState()
+
+  const getData = async () => {
+   
+    let data = await fetch("https://jsonplaceholder.typicode.com/users").then((data) => {
+      (data.json().then(res => {
+        console.log(res)
+        let arreglo = []
+        for (let i = 0; i < res.length; i++) {
+          let user = { username: res[i].username, street: res[i].address.street }
+          console.log(user)
+          arreglo.push(user)
+        }
+        console.log("Arreglo", arreglo)
+        setDatos(arreglo)
+      }))
+    })
+  }
+
+  useEffect(() => {
+    getData()
+  },[]
+  )
+  
+  console.log(datos)
+
+ 
+
   return (
     <div className="App">
       <h2>¡Hola programador!</h2>
--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 
 export default function App() {
+  const [task, setTask] = useState("")
+
+  const addTask = (e) => {
+    console.log(e)
+    setTask(e)
+  }
 
   return (
     <div className="App">
       <h2>¡Hola programador!</h2>
+      <div className="input-container">
+        <input placeholder="Ingresa una nueva tarea" className="input" onChange={(e)=> setTask(e.target.value)} value={task} ></input>
+        <button onClick={addTask}>Agregar</button>
+      </div>
+      <div>
+
+      </div>
     </div>
   );
 }
