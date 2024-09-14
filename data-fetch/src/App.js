3e9a8e72b3ae66f128b6138293a2691fe6a11c6c
Juan Pablo Loaiza Martínez <juanpa@MacBook-Air-de-Juan.local>
2024-09-13 18:20:31 -0700

    Agrego prueba fetch de Felix Castro

--- data-fetch/src/App.js
+++ data-fetch/src/App.js
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 
+
+
+  
 export default function App() {
-  return (
+  const [usuarios, setUsuarios] = useState([])
+
+  async function getData() {
+  const data = await (await fetch("https://jsonplaceholder.typicode.com/users")).json()
+  
+  setUsuarios(data)
+  
+}
+  
+  console.log("boton: ", usuarios);
+  
+  return (<>
     <div className="App">
       <h2>¡Hola programador!</h2>
-      <button>HAZ FETCH</button>
+      <button onClick={getData}>HAZ FETCH</button>
     </div>
+   <div>
+      
+    </div>
+    </>
   );
 }
